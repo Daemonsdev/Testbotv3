733bfad8bbe4dcd44bc28b75ea342f1dd63e98a4
XgHost say <146603842+Daemonsdev@users.noreply.github.com>
2024-10-22 13:33:15 +0800

    Update handlePostback.js

--- handles/handlePostback.js
+++ handles/handlePostback.js
@@ -4,8 +4,6 @@ function handlePostback(event, pageAccessToken) {
   const senderId = event.sender.id;
   const payload = event.postback.payload;
 
-  // Send a message back to the sender
-  sendMessage(senderId, { text: `You sent a postback with payload: ${payload}` }, pageAccessToken);
 }
 
 module.exports = { handlePostback };
